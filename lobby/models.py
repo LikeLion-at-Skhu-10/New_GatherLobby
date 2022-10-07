c409d13275e79e932d518f8606c7e514f8308e2a
yeeun0702 <kimyeeun0702@naver.com>
2022-10-07 18:39:25 +0900

    CRUD

--- lobby/models.py
+++ lobby/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Lobby(models.Model):
+    title = models.CharField(max_length=200)
+    area = models.TextField(max_length=20)
+    member_number = models.IntegerField()
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    
+
+    def __str__(self):
+        return self.title
+
+class Reply(models.Model):
+    def __str__(self):
+        return self.text
+
+    lobby_id = models.ForeignKey(Lobby, on_delete=models.CASCADE, related_name='replys',null = True)
+    text = models.CharField(max_length=100)
+
